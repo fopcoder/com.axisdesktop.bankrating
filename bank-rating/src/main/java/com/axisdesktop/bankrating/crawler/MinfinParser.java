b272971a7efc401614ba855ceaade1bd1d175d39
fxcoder <barabass@gmail.com>
2015-12-17 01:17:30 +0200

    tests, jsoup

--- bank-rating/src/main/java/com/axisdesktop/bankrating/crawler/MinfinParser.java
+++ bank-rating/src/main/java/com/axisdesktop/bankrating/crawler/MinfinParser.java
@@ -2,18 +2,30 @@ package com.axisdesktop.bankrating.crawler;
 
 import java.util.Map;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 public class MinfinParser implements Parser {
 	private String[] raw;
 
-	public MinfinParser( String[] raw ) {
+	public MinfinParser( String... raw ) {
+		if( raw == null ) throw new NullPointerException( "MinfinParser( String.. raw ), raw == null" );
 		this.raw = raw;
 	}
 
 	@Override
 	public Map<String, String> parse() {
+		Document doc;
 
 		for( String s : raw ) {
+			doc = Jsoup.parseBodyFragment( s );
 
+			if( true ) {
+
+			}
+			else {
+
+			}
 		}
 
 		return null;
---
+++ bank-rating/src/test/java/com/axisdesktop/bankrating/test/MinfinParserTest.java
@@ -0,0 +1,88 @@
+package com.axisdesktop.bankrating.test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Map;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import com.axisdesktop.bankrating.crawler.MinfinParser;
+import com.axisdesktop.bankrating.crawler.Parser;
+
+public class MinfinParserTest {
+	public static String indexHtml;
+	public static String bankHtml;
+
+	// @Rule
+	// public final ExpectedException thrown = ExpectedException.none();
+
+	@BeforeClass
+	public static void setup() throws IOException {
+		InputStream is = new ClassPathResource( "/html/minfin_index.html" ).getInputStream();
+		BufferedReader br = new BufferedReader( new InputStreamReader( is ) );
+
+		StringBuilder buf = new StringBuilder();
+		String line;
+
+		while( ( line = br.readLine() ) != null ) {
+			buf.append( line );
+		}
+
+		indexHtml = buf.toString();
+
+		is = new ClassPathResource( "/html/minfin_bank.html" ).getInputStream();
+		br = new BufferedReader( new InputStreamReader( is ) );
+
+		buf = new StringBuilder();
+
+		while( ( line = br.readLine() ) != null ) {
+			buf.append( line );
+		}
+
+		bankHtml = buf.toString();
+
+		br.close();
+		is.close();
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		indexHtml = null;
+		bankHtml = null;
+	}
+
+	@Test( expected = NullPointerException.class )
+	public void shouldThrowNullPointerException() {
+		new MinfinParser( null );
+	}
+
+	@Test
+	public void shouldMinfinParserIndexBankNotNull() {
+		assertNotNull( "new MinfinParser( indexHtml, bankHtml ) should not be null", new MinfinParser( indexHtml,
+				bankHtml ) );
+		assertNotNull( "new MinfinParser( indexHtml ) should not be null", new MinfinParser( indexHtml ) );
+		assertNotNull( "new MinfinParser(  bankHtml ) should not be null", new MinfinParser( bankHtml ) );
+	}
+
+	@Test
+	public void shouldMinfinParserImplementsParser() {
+		assertTrue( "new MinfinParser(  bankHtml ) should implements Parser",
+				new MinfinParser( bankHtml ) instanceof Parser );
+	}
+
+	@Test
+	public void shouldMinfinParserIndexParse() {
+		Parser parser = new MinfinParser( indexHtml );
+		Map params = parser.parse();
+
+		assertNotNull( "parser.parse() should be not null", params );
+	}
+}
